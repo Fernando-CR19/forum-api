9d960708e8a5da17b66e70cfd44ba4bfd8490c1a
Fernando-CR19 <fernandocr019@gmail.com>
2025-04-01 18:21:06 -0300

    feat: finish api

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Prisma, User as UserModel } from '@prisma/client';
+import { UserService } from './user.service';
+import { NotFoundException } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService) { }
+
+    @Post()
+    async signupUser(
+        @Body() userData: Prisma.UserCreateInput,
+    ): Promise<UserModel> {
+        return this.userService.createUser(userData);
+    }
+
+    @Get(':id')
+    async getUser(@Param('id') id: string): Promise<UserModel> {
+        const user = await this.userService.user({ id: Number(id) });
+        if (!user) {
+            throw new NotFoundException(`User with ID ${id} not found`);
+        }
+        return user;
+    }
+
+    @Put()
+    async updateUser(
+        @Body() userData: Prisma.UserUpdateInput,
+        @Param('id') id: string,
+    ): Promise<UserModel> {
+        return this.userService.updateUser({ where: { id: Number(id) }, data: userData });
+    }
+
+    @Delete(':id')
+    async deleteUser(@Param('id') id: string): Promise<UserModel> {
+        return this.userService.deleteUser({ id: Number(id) })
+    }
+}
