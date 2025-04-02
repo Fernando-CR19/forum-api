30347653adbafafc08665903efd0f781ee8d522a
Fernando-CR19 <fernandocr019@gmail.com>
2025-04-02 17:58:58 -0300

    feat: add a guardian to requests

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { Prisma, User as UserModel } from '@prisma/client';
 import { UserService } from './user.service';
 import { NotFoundException } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('user')
 export class UserController {
@@ -14,6 +15,7 @@ export class UserController {
         return this.userService.createUser(userData);
     }
 
+    @UseGuards(AuthGuard)
     @Get(':id')
     async getUser(@Param('id') id: string): Promise<UserModel> {
         const user = await this.userService.user({ id: Number(id) });
@@ -23,6 +25,7 @@ export class UserController {
         return user;
     }
 
+    @UseGuards(AuthGuard)
     @Put()
     async updateUser(
         @Body() userData: Prisma.UserUpdateInput,
@@ -31,6 +34,7 @@ export class UserController {
         return this.userService.updateUser({ where: { id: Number(id) }, data: userData });
     }
 
+    @UseGuards(AuthGuard)
     @Delete(':id')
     async deleteUser(@Param('id') id: string): Promise<UserModel> {
         return this.userService.deleteUser({ id: Number(id) })
