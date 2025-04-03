de4c87d5548d8087bfe251634cd5b393d61f7cff
Fernando-CR19 <fernandocr019@gmail.com>
2025-04-02 21:11:08 -0300

    feat: entity.ts

--- src/answers/entities/answer.entity.ts
+++ src/answers/entities/answer.entity.ts
@@ -1,1 +1,14 @@
-export class Answer {}
+import { Answers } from "@prisma/client";
+import { User } from "src/user/entities/user.entity";
+import { Question } from "src/questions/entities/question.entity";
+
+export class Answer implements Answers {
+    id: number;
+    body: string;
+    createdAt: Date;
+    updatedAt: Date;
+    userId: number;
+    questionsId: number;
+    user: User;
+    question: Question;
+}
--- src/questions/entities/question.entity.ts
+++ src/questions/entities/question.entity.ts
@@ -1,1 +1,11 @@
-export class Question {}
+import { Questions } from "@prisma/client";
+
+export class Question implements Questions {
+    id: number;
+    title: string;
+    body: string;
+    userId: number;
+    createdAt: Date;
+    updatedAt: Date;
+    answersCount: number;
+}
